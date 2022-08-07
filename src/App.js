824c4d8d5c109670c54b74cda4ae1aa5fecfb3ff
DIVYANSH856 <email.divyansh.kohli@gmail.com>
2022-08-07 09:30:46 +0530

    adding searching and filtering in our web while storing original data

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import { Component  } from "react";
 class App extends Component{
   constructor(){  
     super();
+
     this.state={
-      monsters:[ ] 
+      monsters:[ ] ,
+      searchField: []
     };
+    
+    console.log('constructor');
   }
 
 //  a class component specifically that needs to leverage some kind of API call in order to get data that
@@ -16,11 +20,14 @@ class App extends Component{
 // mount lifecycle method.
 
 componentDidMount(){
+  console.log('componentDidMount');
   fetch('https://jsonplaceholder.typicode.com/users')
   .then((response)=>response.json())
   .then((users)=>this.setState(
     ()=>{
-      return {monsters:users};
+      return {
+        monsters:users
+      };
     },
     ()=>{
       console.log(this.state);
@@ -28,10 +35,26 @@ componentDidMount(){
 }
 
 render() {
+  console.log('render');
+  const filteredMonsters = this.state.monsters.filter((monster)=>{ 
+    return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+  });
+  
   return (
     <div className="App">
-      {
-      this.state.monsters.map((monster)=>{
+      <input 
+      className='search-box' 
+      type='search'
+      placeholder='Search monsters' 
+      onChange={(event)=>{
+        const searchField = event.target.value.toLocaleLowerCase();
+        this.setState(
+          ()=>{
+           return {searchField};
+        });}
+      }
+        />
+      { filteredMonsters.map((monster)=>{
         return<div key={monster.id}> 
         <h1 >{monster.name}</h1>
         </div>
